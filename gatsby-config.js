79a64a1dc7c207105f17f6a4270cf57e8d232827
Thomas Tastet <hello@thomas-tastet.pro>
2020-03-21 18:55:20 +0100

    v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,51 @@
 require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
-});
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-    siteMetadata: {
-        title: `New-York`,
-        description: ``,
-        author: `@thomast_404`,
-        url: `https://new-york-with-gatsby.netlify.com`
+  siteMetadata: {
+    title: `New-York`,
+    description: ``,
+    author: `@thomast_404`,
+    url: `https://new-york-with-gatsby.netlify.com`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-demo-new-york`,
+        short_name: `new-york`,
+        start_url: `/`,
+        background_color: `#cfcbc8`,
+        theme_color: `#cfcbc8`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        schemas: {
+          page: require("./src/schemas/page.json"),
         },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-demo-new-york`,
-                short_name: `new-york`,
-                start_url: `/`,
-                background_color: `#cfcbc8`,
-                theme_color: `#cfcbc8`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`,
-            },
+        repositoryName: process.env.PRISMIC_REPO,
+        accessToken: process.env.PRISMIC_TOKEN,
+        shouldDownloadImage: ({ node, key, value }) => {
+          // Return true to download the image or false to skip.
+          return true
         },
-        `gatsby-plugin-styled-components`,
-        {
-            resolve: 'gatsby-source-prismic',
-            options: {
-                schemas: {
-                    page: require('./src/schemas/page.json')
-                },
-                repositoryName: process.env.PRISMIC_REPO,
-                accessToken: process.env.PRISMIC_TOKEN
-            }
-        }
-    ],
-};
+      },
+    },
+  ],
+}
---
+++ src/pages/preview.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react"
+import { usePrismicPreview } from "gatsby-source-prismic"
+
+import PageTemplate from "../templates/page"
+
+// Note that the `location` prop is taken and provided to the `usePrismicPreview` hook.
+const PreviewPage = ({ location }) => {
+  const [prevData, setData] = useState(null)
+  const { isPreview, previewData, path } = usePrismicPreview({
+    // The repositoryName value from your `gatsby-config.js`.
+    repositoryName: "new-york",
+  })
+
+  // This useEffect runs when values from usePrismicPreview update. When
+  // preview data is available, this will save the data globally and redirect to
+  // the previewed document's page.
+  useEffect(() => {
+    // If this is not a preview, skip.
+    //   null = Not yet determined if previewing.
+    //   true = Preview is available.
+    //   false = Preview is not available.
+    if (isPreview === false) return
+
+    // Save the preview data to somewhere globally accessible. This could be
+    // something like a global Redux store or React context.
+    //
+    // We'll just put it on window.
+    setData(previewData)
+    window.__PRISMIC_PREVIEW_DATA__ = previewData
+  }, [isPreview, previewData, path, setData])
+
+  // Tell the user if this is not a preview.
+  if (isPreview === false) return <div>Not a preview!</div>
+
+  return prevData ? (
+    prevData.prismicPost ? (
+      <PageTemplate data={prevData.prismicPost} />
+    ) : (
+      <div>error...</div>
+    )
+  ) : (
+    <div>Loading preview...</div>
+  )
+}
+
+export default PreviewPage
